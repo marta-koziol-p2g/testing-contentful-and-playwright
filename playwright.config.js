a030e23b0632f3f17a3a709798972f3eb3a5d4f4
Marta Koziol <marta.koziol@parcel2go.com>
2023-01-07 12:40:49 +0000

    Some changes

--- playwright.config.js
+++ playwright.config.js
@@ -16,17 +16,18 @@ const config = {
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:3000',
-    reuseExistingServer: true
+    reuseExistingServer: true,
+    timeout: 5 * 60 * 1000,
   },
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 5 * 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 50000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -41,7 +42,7 @@ const config = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
+    actionTimeout: 50000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:3000',
 
--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test';
+import { expect, test} from '@playwright/test';
 // import test from './next-fixture';
 
 test.beforeEach(async ({ page }) => {
@@ -7,20 +7,23 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Blog Homepage', () => {
   test('Should display correct h1 title', async ({ page }) => {
+    // await page.goto(`http://localhost:3000`);
     const h1 = await page.textContent('h1');
     expect(h1).toBe('Blog.');
   });
 
   // test('Blog', async ({ page }) => {
   //   await page.getByText('Learning Playwright Workflows').click();
+  //   await page.waitForLoadState('networkidle');
   //   const h1 = await page.textContent('h1');
   //   expect(h1).toBe('Learning Playwright Workflows');
   // });
 
-  test('Blog2', async ({ page}) => {
-    await page.goto(`http://localhost:3000/posts/learning-playwright-workflows`);
-       const h1 = await page.textContent('h1');
-    expect(h1).toBe('Learning Playwright Workflows');
-  });
+  // test('Blog2', async ({ page}) => {
+  //   await page.goto(`/posts/learning-playwright-workflows`);
+  //   // await page.waitForLoadState('networkidle');
+  //   const h1 = await page.textContent('h1');
+  //   expect(h1).toBe('Learning Playwright Workflows');
+  // });
 
 });
