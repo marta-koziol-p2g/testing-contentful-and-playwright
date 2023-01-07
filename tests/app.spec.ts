9fcc213bb78f0a407bc105c60ae479df179054bf
Marta Koziol <marta.koziol@parcel2go.com>
2023-01-07 15:08:46 +0000

    test

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -19,11 +19,15 @@ test.describe('Blog Homepage', () => {
   //   expect(h1).toBe('Learning Playwright Workflows');
   // });
 
-  // test('Blog2', async ({ page}) => {
-  //   await page.goto(`/posts/learning-playwright-workflows`);
-  //   // await page.waitForLoadState('networkidle');
-  //   const h1 = await page.textContent('h1');
-  //   expect(h1).toBe('Learning Playwright Workflows');
-  // });
+  test('Blog2', async ({ page}) => {
+    await page.goto(`/posts/learning-playwright-workflows`);
+    page.route('/posts/learning-playwright-workflows', (route, request) => {
+      console.log(request.url());
+      route.continue();
+  }); 
+    // await page.waitForLoadState('networkidle');
+    const h1 = await page.textContent('h1');
+    expect(h1).toBe('Learning Playwright Workflows');
+  });
 
 });
