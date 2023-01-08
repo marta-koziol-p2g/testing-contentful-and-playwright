ef78bb83a914a5973f728c64277a83bb9f4a197f
Marta Koziol <marta.koziol@parcel2go.com>
2023-01-08 16:23:35 +0000

    Uninstall cypress

--- tests.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.getByText('Learning Playwright Workflows').click();
-  await page.goto('http://localhost:3000/posts/learning-playwright-workflows/');
-});
