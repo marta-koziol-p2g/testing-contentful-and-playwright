7ecf96effe12310ea2a61bc7e5b40d3e5a7e8ff0
Marta Koziol <marta.koziol@parcel2go.com>
2023-01-08 16:36:05 +0000

    Add more tests

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -13,19 +13,19 @@ test.describe('Blog Homepage', () => {
     expect(h1).toBe('Blog.');
   });
 
-  // test('Blog', async ({ page }) => {
-  //   await page.getByText('Learning Playwright Workflows').click();
-  //   await page.waitForLoadState('networkidle');
-  //   const h1 = await page.textContent('h1');
-  //   expect(h1).toBe('Learning Playwright Workflows');
-  // });
+  test('should access Blog 1 by click', async ({ page }) => {
+    await page.getByText('Learning Playwright Workflows').click();
+    await page.waitForLoadState('networkidle');
+    const h1 = await page.textContent('h1');
+    expect(h1).toBe('Learning Playwright Workflows');
+  });
 
-  // test('Blog2', async ({page},...rest) => {
+  test('should access Blog2 by redirect', async ({page},...rest) => {
 
-  //   await page.goto(`/ui-static/homepage/`);
-  //   await page.waitForLoadState('networkidle');
-  //   const h1 = await page.textContent('h1');
-  //   expect(h1).toBe('Learning Playwright Workflows');
-  // });
+    await page.goto(`/posts/learning-contentful`);
+    await page.waitForLoadState('networkidle');
+    const h1 = await page.textContent('h1');
+    expect(h1).toBe('Learning Contentful');
+  });
 
 });
