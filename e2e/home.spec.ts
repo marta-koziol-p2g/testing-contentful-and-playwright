a9c5b4fea4133902f3eaaa2a572fe155a8783748
Marta Koziol <marta.koziol@parcel2go.com>
2023-01-08 16:44:36 +0000

    Modify tests

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,21 +1,19 @@
 import { expect, test, webkit} from '@playwright/test';
 // import test from './next-fixture';
 
-// test.beforeEach(async ({ page }) => {
-//   await page.goto(`http://localhost:3000`);
-// });
+test.beforeEach(async ({ page, baseURL }) => {
+  await page.goto(baseURL || 'http://localhost:3000');
+});
 
 test.describe('Blog Homepage', () => {
   test('Should display correct h1 title', async ({page, baseURL}) => {
-    await page.goto(baseURL || 'http://localhost:3000');
-
     const h1 = await page.textContent('h1');
     expect(h1).toBe('Blog.');
   });
 
   test('should access Blog 1 by click', async ({ page }) => {
     await page.getByText('Learning Playwright Workflows').click();
-    await page.waitForLoadState('networkidle');
+    await page.goto(`posts/learning-playwright-workflows`);
     const h1 = await page.textContent('h1');
     expect(h1).toBe('Learning Playwright Workflows');
   });
@@ -23,7 +21,6 @@ test.describe('Blog Homepage', () => {
   test('should access Blog2 by redirect', async ({page},...rest) => {
 
     await page.goto(`/posts/learning-contentful`);
-    await page.waitForLoadState('networkidle');
     const h1 = await page.textContent('h1');
     expect(h1).toBe('Learning Contentful');
   });
