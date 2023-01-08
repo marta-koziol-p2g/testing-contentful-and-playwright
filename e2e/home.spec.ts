7e5699202f1a2357df089ddd1f08ad0f3bd224b0
Marta Koziol <marta.koziol@parcel2go.com>
2023-01-08 17:29:39 +0000

    Comment out safari

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -13,7 +13,7 @@ test.describe('Blog Homepage', () => {
 
   test('should access Blog 1 by click', async ({ page }) => {
     await page.getByText('Learning Playwright Workflows').click();
-    await page.goto(`posts/learning-playwright-workflows`);
+    // await page.goto(`posts/learning-playwright-workflows`);
     const h1 = await page.textContent('h1');
     expect(h1).toBe('Learning Playwright Workflows');
   });
