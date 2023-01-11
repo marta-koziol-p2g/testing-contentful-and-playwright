a907e12a71a26be4c0c1bf3c987292aba3bdafc4
Marta Koziol <marta.koziol@parcel2go.com>
2023-01-11 13:58:50 +0000

    Test nexted route

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -11,17 +11,24 @@ test.describe('Blog Homepage', () => {
     expect(h1).toBe('Blog.');
   });
 
-  // test('should access Blog 1 by redirect', async ({ page }) => {
-  //   await page.goto(`posts/learning-playwright-workflows`);
-  //   const h1 = await page.textContent('h1');
-  //   expect(h1).toBe('Learning Playwright Workflows');
-  // });
+  test('should access Blog 1 by redirect', async ({ page }) => {
+    await page.goto(`posts/learning-playwright-workflows`);
+    const h1 = await page.textContent('h1');
+    expect(h1).toBe('Learning Playwright Workflows');
+  });
 
-  test('should access Blog2 by redirect', async ({page},...rest) => {
+  test('should ', async ({page},...rest) => {
 
     await page.goto(`/posts`);
     const h1 = await page.textContent('h1');
     expect(h1).toBe('This is H1');
+  });
+
+  test('should  author', async ({page},...rest) => {
+
+    await page.goto(`/posts/authors`);
+    const h1 = await page.textContent('h2');
+    expect(h1).toBe('This is H2 in Authors');
   });
 
 });
---
+++ pages/posts/authors/index.js
@@ -0,0 +1,7 @@
+
+export default function Index() {
+
+  return (
+    <h2>This is H2 in Authors</h2>
+  )
+}
